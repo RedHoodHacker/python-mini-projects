3a46fd624fd5d4321d68c01c0379964e8669219a
RedHoodHacker <69258535+RedHoodHacker@users.noreply.github.com>
2022-04-20 21:43:25 -0700

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+https://data-flair.training/blogs/python-mad-libs-generator-game/
+
+  
+from tk import * 
+
+root = Tk()
+root.geometry('300x300')
+root.title('Mad Libs generator')
+Label(root, text= 'Mad Libs Generator \n enjoy!', font = 'arial 20 bold').pack()
+Label(root, text= 'Click any one: ', font = 'arial 15 bold').place(x=40, y=80)
+
